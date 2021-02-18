bd64c68d02bac24f1423f8da4c8ec0b3ecef57a4
Dmitri <56200546+Syritx@users.noreply.github.com>
2021-02-18 09:11:44 -0500

    added notes

--- api.py
+++ api.py
@@ -21,6 +21,7 @@ def togglePart():
     global isPartActive, message, json_file, logs
     isPartActive = not isPartActive
     
+    # writing a new log by a button press
     if isPartActive:
         message = 'active'
         log = str(datetime.now().strftime("%H:%M:%S"))+' Status: active'
@@ -30,20 +31,25 @@ def togglePart():
         message = 'inactive'
         log = str(datetime.now().strftime("%H:%M:%S"))+' Status: inactive'
         logs.append(log)
-
+    
+    # reversing logs (recent to oldest)
     new_logs = []
     for x in range(len(logs)-1, 0, -1):
         new_logs.append(logs[x])
-
-    json_file = '{ \"active\": '+str(isPartActive).lower()+' }'
-
+    
+    # gets 10 recent logs
     completed_logs = []
     for x in range(len(new_logs)-1):
         if (len(new_logs)-x > len(new_logs)-10):
             completed_logs.append(new_logs[x])
-
+    
+    # creating json file
+    json_file = '{ \"active\": '+str(isPartActive).lower()+' }'
+    
+    # rendering html
     return render_template('index.html',part_active='Status: ' + message.upper(), content=completed_logs)
 
+# API page
 @app.route('/api')
 def api():
     global json_file
